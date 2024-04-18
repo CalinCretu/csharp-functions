d1224acca1624cf2f3d3d1f6ae6dbe1b63378717
CalinCretu <calincretu1412@gmail.com>
2024-04-18 14:28:46 +0200

    ADD - StampaArray in forma (elemento1 ... )

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            void StampaArray(int[] array)       // Dichiaro la funzione con void poiche' puo' non deve restituire un valore specifico
+            {                                   // Passo anche il parametro int[] array
+                Console.Write("(");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    StampaElemento();
+
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+
+                        StampaVirgolaESpazio();
+                }
+                Console.Write(")");
+            }
+            void StampaVirgolaESpazio()         // Dichiaro la funzione void come sopra
+            {
+                Console.Write(", ");
+            }
+            void StampaElemento()
+            {
+                Console.Write("elemento ");
+            }
+
+            int[] numeri = { 1, 2, 3, 4, 5, 6 };    // Creo l'array a mio piacimento
+            StampaArray(numeri);                    // Richiamo la fuzione stampaArray passando il mio array come parametro
         }
     }
 }
